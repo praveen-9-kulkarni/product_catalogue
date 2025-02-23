a0bf78cf8b0308b194da83f91366b1ca9c52a1a1
Praveen Kumar K <praveen.9.kulkarni@gmail.com>
2025-02-23 20:59:35 +0530

    Created a fully-functional Product entity class that: - Maps to a database table named 'product' - Has automatic getter/setter methods - Has a primary key that auto-increments - Can be easily persisted and retrieved using JPA/Hibernate

---
+++ src/main/java/com/learn/product_catalogue/product_catalogue/model/Product.java
@@ -0,0 +1,21 @@
+package com.learn.product_catalogue.product_catalogue.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Product {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String category;
+    private String imageUrl;
+}
