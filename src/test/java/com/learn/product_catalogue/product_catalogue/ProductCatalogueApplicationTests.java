e3758a695dcc125eb0b2d07b59318a899489c25a
Praveen Kumar K <praveen.9.kulkarni@gmail.com>
2025-02-23 20:03:18 +0530

    first commit

---
+++ src/test/java/com/learn/product_catalogue/product_catalogue/ProductCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.product_catalogue.product_catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
